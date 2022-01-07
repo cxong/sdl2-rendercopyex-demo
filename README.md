3aed01ca672d3bef5f1201d74f0bd82d6d0a9b50
Cong <congusbongus@gmail.com>
2022-01-07 13:18:20 +1100

    Add wipe and slide examples

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Demonstrate stuff you can do with SDL2_RenderCopyEx()
 - Rotation
 - Scaling
 - Flipping
+- Wiping
+- Sliding
 
 ![](screenshot.png)
 
@@ -12,3 +14,8 @@ https://opengameart.org/content/unsealed-shura-battle-gfx
 Laura 'Yamilian' Armendariz
 http://creativecommons.org/licenses/by-sa/3.0/
 http://www.gnu.org/licenses/gpl-3.0.html
+
+door.png, frame.png
+https://opengameart.org/content/test-door
+bradyelavich
+http://creativecommons.org/publicdomain/zero/1.0/
--- main.c
+++ main.c
@@ -3,45 +3,62 @@
 
 #include <SDL_image.h>
 
-int render(
-	SDL_Renderer *renderer, SDL_Surface *s, SDL_Texture *t,
-	int x, int y, double angle, double sx, double sy, SDL_RendererFlip flip)
+int RenderScaleFlip(
+	SDL_Renderer* renderer, SDL_Texture* t,
+	int x, int y, int w, int h, double angle, double sx, double sy, SDL_RendererFlip flip)
 {
 	SDL_Rect dst;
-	int res;
-	dst.x = x - (s->w * sx) / 2;
-	dst.y = y - (s->h * sy) / 2;
-	dst.w = (int)(s->w * sx);
-	dst.h = (int)(s->h * sy);
-	res = SDL_RenderCopyEx(renderer, t, NULL, &dst, angle, NULL, flip);
-	if (res != 0)
-	{
-		return res;
-	}
-	return SDL_RenderDrawPoint(renderer, x, y);
+	dst.x = x - (w * sx) / 2;
+	dst.y = y - (h * sy) / 2;
+	dst.w = (int)(w * sx);
+	dst.h = (int)(h * sy);
+	return SDL_RenderCopyEx(renderer, t, NULL, &dst, angle, NULL, flip);
 }
 
-int main(int argc, char *argv[])
+SDL_Texture* LoadTexture(SDL_Renderer* renderer, const char* filename, int* w, int* h)
 {
-	SDL_Surface *s = NULL;
-	SDL_Texture *t = NULL;
-	SDL_Window *window = NULL;
-	SDL_Renderer *renderer = NULL;
-	int width = 640;
-	int height = 320;
+	SDL_Texture* t = NULL;
+
+	// Load image from file
+	SDL_Surface* s = IMG_Load(filename);
+	if (s == NULL)
+	{
+		printf("Failed to load image from file %s\n", filename);
+		goto bail;
+	}
+
+	// Convert to texture
+	t = SDL_CreateTextureFromSurface(renderer, s);
+	if (t == NULL)
+	{
+		printf("Failed to create texture from surface: %s\n", SDL_GetError());
+		goto bail;
+	}
+
+	*w = s->w;
+	*h = s->h;
+
+bail:
+	return t;
+}
+
+int main(int argc, char* argv[])
+{
+	SDL_Texture* tShura = NULL;
+	int wShura, hShura;
+	SDL_Texture* tDoor = NULL;
+	int wDoor, hDoor;
+	SDL_Texture* tFrame = NULL;
+	int wFrame, hFrame;
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	int width = 520;
+	int height = 700;
 
 	// Init SDL
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
 		printf("SDL_Init error: %s\n", SDL_GetError());
-		goto bail;
-	}
-
-	// Load image from file
-	s = IMG_Load("shura.png");
-	if (s == NULL)
-	{
-		printf("Failed to load image from file %s\n", argv[1]);
 		goto bail;
 	}
 
@@ -62,11 +79,20 @@ int main(int argc, char *argv[])
 	}
 	SDL_RenderSetLogicalSize(renderer, width, height);
 
-	// Convert to texture
-	t = SDL_CreateTextureFromSurface(renderer, s);
-	if (t == NULL)
+	// Load textures from file
+	tShura = LoadTexture(renderer, "shura.png", &wShura, &hShura);
+	if (tShura == NULL)
 	{
-		printf("Failed to convert surface: %s\n", SDL_GetError());
+		goto bail;
+	}
+	tDoor = LoadTexture(renderer, "door.png", &wDoor, &hDoor);
+	if (tDoor == NULL)
+	{
+		goto bail;
+	}
+	tFrame = LoadTexture(renderer, "frame.png", &wFrame, &hFrame);
+	if (tFrame == NULL)
+	{
 		goto bail;
 	}
 
@@ -77,24 +103,102 @@ int main(int argc, char *argv[])
 		goto bail;
 	}
 
-	// Blit the texture to screen
+	// Render the textures
 	for (int i = 0; i < 5; i++)
 	{
-		int x = 50 + 100 * i;
+		int x = 60 + 100 * i;
 		double angle = 90 / 4 * i;
-		if (render(renderer, s, t, x, 50, angle, 1.0, 1.0, SDL_FLIP_NONE) != 0)
+
+		// rotate
+		if (RenderScaleFlip(renderer, tShura, x, 50, wShura, hShura, angle, 1.0, 1.0, SDL_FLIP_NONE) != 0)
 		{
-			printf("Failed to blit surface: %s\n", SDL_GetError());
+			printf("Failed to render texture: %s\n", SDL_GetError());
 			goto bail;
 		}
-		if (render(renderer, s, t, x, 150, angle, 0.5, 1.5, SDL_FLIP_NONE) != 0)
+		// rotate and scale
+		if (RenderScaleFlip(renderer, tShura, x, 150, wShura, hShura, angle, 0.5, 1.5, SDL_FLIP_NONE) != 0)
 		{
-			printf("Failed to blit surface: %s\n", SDL_GetError());
+			printf("Failed to render texture: %s\n", SDL_GetError());
 			goto bail;
 		}
-		if (render(renderer, s, t, x, 250, angle, 1.0, 1.0, SDL_FLIP_HORIZONTAL) != 0)
+		// rotate and flip
+		if (RenderScaleFlip(renderer, tShura, x, 250, wShura, hShura, angle, 1.0, 1.0, SDL_FLIP_HORIZONTAL) != 0)
 		{
-			printf("Failed to blit surface: %s\n", SDL_GetError());
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+
+		SDL_Rect src, dst;
+
+		// wipe right
+		if (RenderScaleFlip(renderer, tFrame, x, 350, wFrame, hFrame, 0.0, 1.0, 1.0, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+		src.x = wDoor * i / 5;
+		src.y = 0;
+		src.w = dst.w = wDoor - src.x;
+		src.h = dst.h = hDoor;
+		dst.x = x + src.x - wDoor / 2;
+		dst.y = 354 - hDoor / 2;
+		if (SDL_RenderCopyEx(renderer, tDoor, &src, &dst, 0.0, NULL, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+
+		// wipe up
+		if (RenderScaleFlip(renderer, tFrame, x, 450, wFrame, hFrame, 0.0, 1.0, 1.0, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+		src.x = 0;
+		src.y = 0;
+		src.w = dst.w = wDoor;
+		src.h = dst.h = hDoor - hDoor * i / 5;
+		dst.x = x - wDoor / 2;
+		dst.y = 454 - hDoor / 2;
+		if (SDL_RenderCopyEx(renderer, tDoor, &src, &dst, 0.0, NULL, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+
+		// slide right
+		if (RenderScaleFlip(renderer, tFrame, x, 550, wFrame, hFrame, 0.0, 1.0, 1.0, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+		src.x = 0;
+		src.y = 0;
+		src.w = dst.w = wDoor - wDoor * i / 5;
+		src.h = dst.h = hDoor;
+		dst.x = x + wDoor * i / 5 - wDoor / 2;
+		dst.y = 554 - hDoor / 2;
+		if (SDL_RenderCopyEx(renderer, tDoor, &src, &dst, 0.0, NULL, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+
+		// slide up
+		if (RenderScaleFlip(renderer, tFrame, x, 650, wFrame, hFrame, 0.0, 1.0, 1.0, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
+			goto bail;
+		}
+		src.x = 0;
+		src.y = hDoor * i / 5;
+		src.w = dst.w = wDoor;
+		src.h = dst.h = hDoor - src.y;
+		dst.x = x - wDoor / 2;
+		dst.y = 654 - hDoor / 2;
+		if (SDL_RenderCopyEx(renderer, tDoor, &src, &dst, 0.0, NULL, SDL_FLIP_NONE) != 0)
+		{
+			printf("Failed to render texture: %s\n", SDL_GetError());
 			goto bail;
 		}
 	}
@@ -119,8 +223,9 @@ int main(int argc, char *argv[])
 
 
 bail:
-	SDL_DestroyTexture(t);
-	SDL_FreeSurface(s);
+	SDL_DestroyTexture(tShura);
+	SDL_DestroyTexture(tDoor);
+	SDL_DestroyTexture(tFrame);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
